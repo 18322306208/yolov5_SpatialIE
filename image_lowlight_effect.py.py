c3e71f4b7baa750abe3e3e504d27e35ed508e6e1
18322306208 <49777960+18322306208@users.noreply.github.com>
2024-09-10 10:52:50 +1000

    Update image_lowlight_effect.py.py

--- image_lowlight_effect.py.py
+++ image_lowlight_effect.py.py
@@ -3,32 +3,32 @@ import cv2
 import numpy as np
 
 
-# 定义 gamma 变换函数
+# Define the gamma transform function
 def gamma_transform(img, gamma):
     gamma_corrected = np.uint8(cv2.pow(img / 255.0, gamma) * 255)
     return gamma_corrected
 
 
-# 文件夹路径
-folder_path = "D:\dataset/trash_data\low-light\images"
-folder_path_out = "D:\dataset/trash_data\low-light\images_corrected"
+# Folder Path
+folder_path = ""
+folder_path_out = ""
 
-# 获取文件夹中的所有图片文件
+# Get all image files in a folder
 image_files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
 
-# 循环处理每个图片文件
+# Loop through each image
 for image_file in image_files:
-    # 加载图像
+    # Load Image
     img = cv2.imread(os.path.join(folder_path, image_file), cv2.IMREAD_COLOR)
 
-    # 随机生成 gamma 值
+    # Randomly generated gamma values
     gamma = np.random.uniform(1.5, 5)
 
-    # 应用 gamma 变换
+    # Applying the gamma transform
     gamma_corrected_img = gamma_transform(img, gamma)
 
-    # 保存处理后的图像
+    # Saving processed images
     output_file = os.path.join(folder_path_out, image_file)
     cv2.imwrite(output_file, gamma_corrected_img)
 
